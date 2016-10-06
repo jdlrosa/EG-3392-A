5837e4d35de537e2ce31cd44b0bfe7f0f0529fba
Julian <jdlrosa45@gmail.com>
2016-10-06 18:53:39 -0500

    commit

---
+++ Division/src/Division.java
@@ -0,0 +1,48 @@
+/*
+ * Julian De La Rosa
+ * EG3392
+ * Assignment 6 Division
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Division {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		int A = 0;
+		int B;
+		
+		try{
+		System.out.println("Please enter the first integer");
+		A = kb.nextInt();
+		
+		System.out.println("Please enter the second integer");
+		B = kb.nextInt();
+		
+		System.out.println(A / B);
+		}
+		
+		catch(InputMismatchException e){
+			System.out.println(e.getMessage() + "exception");
+			System.out.println("Please reenter the integers");
+			kb.nextLine();
+			A = kb.nextInt();
+			B = kb.nextInt();
+			System.out.println(A / B);
+		}
+		
+		catch(ArithmeticException e){
+			System.out.println(e.getMessage() + "exception");
+			System.out.println("Please reenter the second integer" );
+			B = kb.nextInt();
+			System.out.println(A / B);
+		}
+		
+		
+		
+		kb.close();
+
+	}
+
+}
